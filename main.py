ebaecfdb13fc8db6105c669c517f8f4e7764f3e1
quintendebeste <quintenvedder@gmail.com>
2023-02-01 19:25:31 +0100

    added hitbox en mouse click detection on pieces

--- main.py
+++ main.py
@@ -5,29 +5,58 @@ screen_size = 750,750
 screen = pg.display.set_mode(screen_size)
 pg.font.init()
 playing = True
-def draw_nodes(start_pos,end_pos,line_length,vertical):
-    start_pos = start_pos
-    end_pos = end_pos
-    space_between_nodes = line_length / 8
-    vertical = vertical
-    if(vertical == True):
-        for i in range(10):
-            pg.draw.circle(screen, (255,0,0), (start_pos[0], start_pos[1] + (i * space_between_nodes)), 10)
-            print("vert",start_pos[0], start_pos[1] + (i * space_between_nodes))
-
-    elif(vertical != True):
-        for i in range(10):
-            pg.draw.circle(screen, (255,0,0), (start_pos[0] + (i * space_between_nodes), start_pos[1]), 10)
-            print("hori", start_pos[0], start_pos[1] + (i * space_between_nodes))
-        #sys.exit()
+green = (0,255,0)
+red = (255,0,0)
 
 
+# draws nodes on the same spot as the chesspieces using the "chess_pieces" list for the location of the pieces
+def draw_nodes():
+    offset = 55
+    for row in range(8):
+        for col in range(8):
+            output = chess_grid[row][col]
+            if output != 'Null':
+                if output in chess_pieces:
+                    output = chess_pieces[output]
+                pg.draw.circle(screen, red, (col * 90 + offset + 5, row * 90 + offset - 2), 10)
+
+# checks the position of the mouse and if the position is equal to the position of a node and if its clicked
+def mouse_detection():
+    mouse_pos = pg.mouse.get_pos()
+    for row in range(8):
+        for col in range(8):
+            output = chess_grid[row][col]
+            if output != 'Null':
+                node_x = col * 90 + 55 + 5
+                node_y = row * 90 + 55 - 2
+                node_rect = pg.Rect(node_x - 25, node_y - 25, 50, 50)
+                pg.draw.rect(screen, red, node_rect)
+                if node_rect.collidepoint(mouse_pos):
+                    if pg.mouse.get_pressed()[0]:
+                        if output in chess_pieces:
+                            output = chess_pieces[output]
+                            return output,row,col
+
+# a function wich shows the possible moves according to the clicked piece in the "mouse_detection" function
+def moves_show(piece,row,col):
+    offset = 55
+    piece = piece
+    row = row
+    col = col
+    print(piece,f"location: (row:{row},column:{col})")
+    if(piece == chess_pieces['w_pawn']):
+        for i in range(2):
+            row -= 1
+            pg.draw.circle(screen, green, (col * 90 + offset + 5, row * 90 + offset - 2), 10)
+            print("move")
 
 
 
-#tile fill list
+# font for chesspieces
 set_font = pg.font.match_font("Segoe UI Symbol")
 font = pg.font.Font(set_font, 42)
+
+# a list of the unicode for each chesspiece put in a variable for ease of use
 chess_pieces = {
     'b_checker': u'\u25FB',
     'b_pawn': u'\u265F',
@@ -45,6 +74,7 @@ chess_pieces = {
     'w_queen': u'\u2655'
 }
 
+# this list represents a chess board with the pieces on their locations
 chess_grid = [
     ['b_rook', 'b_knight', 'b_bishop', 'b_queen', 'b_king', 'b_bishop', 'b_knight', 'b_rook'],
     ['b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn'],
@@ -56,6 +86,7 @@ chess_grid = [
     ['w_rook', 'w_knight', 'w_bishop', 'w_queen', 'w_king', 'w_bishop', 'w_knight', 'w_rook']
 ]
 
+# a function for going trough the chess_grid list and placing the pices on the board according to their location in the list
 def draw_pieces():
     offset = 35
     for row in range(8):
@@ -67,6 +98,7 @@ def draw_pieces():
                 piece_surface = font.render(output, True, pg.Color('black'))
                 screen.blit(piece_surface, (col * 90 + offset + 5, row * 90 + offset - 2))
 
+# a function for drawing a chess board in the pygame window for the chesspices to stand on
 def draw_background():
     screen.fill(pg.Color("burlywood2"))
     pg.draw.rect(screen, pg.Color("burlywood4"), pg.Rect(15, 15, 720, 720), 7)
@@ -77,14 +109,13 @@ def draw_background():
         Vend_pos = pg.Vector2((i * 90) + 15, 730)
         line_length = 715
         pg.draw.line(screen, pg.Color("burlywood4"), Vstrt_pos, Vend_pos, 2)
-        draw_nodes(Vstrt_pos,Vend_pos,line_length,True)
 
         #horizontal lines
         Hstrt_pos = pg.Vector2(15, (i * 90) + 15)
         Hend_pos = pg.Vector2(730, (i * 90) + 15)
         pg.draw.line(screen, pg.Color("burlywood4"), Hstrt_pos, Hend_pos, 2)
-        draw_nodes(Hstrt_pos,Hend_pos,line_length,False)
 
+# a function wich represents a basic gameloop that's running all the previous functions one after another
 def game_loop():
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -92,8 +123,14 @@ def game_loop():
 
     draw_background()
     draw_pieces()
+    #draw_nodes()
+    mouse_detection()
+    piece,row,col = mouse_detection()
+    if(piece):
+        moves_show(piece,row,col)
     pg.display.flip()
 
+# a while loop that keeps looping the game_loop function until the window is closed
 while playing:
     mouse = pg.mouse.get_pos()
     game_loop() 
